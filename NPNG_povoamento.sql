f3762850885ca4cccd887c30366b154a20455b00
GuilhermeViveiros <a80524@alunos.uminho.pt>
2018-11-12 17:22:32 +0000

    modelo lógica

--- NPNG_povoamento.sql
+++ NPNG_povoamento.sql
@@ -10,9 +10,11 @@
 -- Base de dados de trabalho
 USE NPNG;
 
+
+        
 -- Povoamento da tabela "Cliente"
 INSERT INTO Cliente
-    (Nome,Nif)
+    (idCliente,Nome,Nif)
     VALUES
         ("André Guilherme","123123123"),
         ("César Augusto","111222333"),
@@ -20,22 +22,20 @@ INSERT INTO Cliente
         ("Angelo André Castro Sousa","101202303"),
         ("Mini j","123456789"),
         ("Marco Dantas","123123789"),
-        ("Carlos Alonso","4332143211"),        
- 		("Daniel GangaMen","1112121234"),
- 		("Gandalf","1999999999"),
- 		("Frodo Baggins","11111111111"),
- 		("Julio Cesar","1987654321"),
- 		("Graca Solha","3987456123"),
- 		("Nuno Valente","2000123123"),
-		("Aragorn","2432143211"),
-		("Smeagol","3123123666"),
-		("Gollum","6666666666");
+        ("Carlos Alonso","433214321"),        
+ 		("Daniel GangaMen","111212124"),
+ 		("Gandalf","999999999"),
+ 		("Frodo Baggins","111111111"),
+ 		("Julio Cesar","987654321"),
+ 		("Graca Solha","398745613"),
+ 		("Nuno Valente","200123123"),
+		("Aragorn","243214321"),
+		("Smeagol","123123666"),
+		("Gollum","666666666");
 
 
-select *from cliente;
 
 -- Povoamento da tabela "Funcionário"
-
 
 INSERT INTO Funcionário
     (Categoria,Email,Nome,Número)
@@ -56,42 +56,51 @@ INSERT INTO Aula
     (Nome, Descrição , Lotação ,idPersonal_Trainer)
     VALUES
         ("Cycling","Aula em bicicleta",10,5),
-        ("Spinning","Aula de spinning",20,6),
-        ("Running","Corrida coletiva",15,7),
-        ("Pilates","Aula de Pilates",4,8),
-		("Karaté","Aula para defesa pessoal",20,1),
-		("Kick-Box","Aula para defesa pessoal",22,8),
-		("Judo","Aula para controlo de mente",14,9); 
+        ("Spinning","Aula de spinning",20,9),
+        ("Running","Corrida coletiva",15,5),
+        ("Pilates","Aula de Pilates",4,9),
+		("Karaté","Aula para defesa pessoal",20,5),
+		("Kick-Box","Aula para defesa pessoal",22,5),
+		("Judo","Aula para controlo de mente",14,5); 
 
-INSERT INTO Máquina
-    (Nome,Descrição,Lotação)
+-- Povoamento da tabela "Máquina"
+
+INSERT INTO Máquina 
+    (Nome,Descrição)
     VALUES
         ("Supino reto","Ajuda a fortalecer o peito"),
         ("Supino inclinado","Ajuda a fortalecer o peito"),
         ("Leg Press","Ajuda a fortalecer as pernas"),
         ("Multi-Task","Ajuda a fortalecer bíceps e tríceps"),
-	 ("Remo","Ajuda a fortalecer a resistência, máquina para aquecimento"),
+		("Remo","Ajuda a fortalecer a resistência, máquina para aquecimento"),
         ("Deep","Ajuda no peito inferior e tríceps"),
         ("Passadeira","Ajuda a fortalecer a resistência"),
         ("Curls","Ajudam a fortalecer os bíceps"),
-("Bicicleta","Ajuda a fortalecer a resistência");
+		("Bicicleta","Ajuda a fortalecer a resistência");
 
 
 
 INSERT INTO Pacote
-    (Nome,idCliente,Personal_Trainer,Nutricionista)
+    (Nome,id_Personal_Trainer,id_Nutricionista,idCliente,Horário_Inicio,Horário_Fim)
     VALUES
-        ("Full Pack",1,9,6),
-        ("Medium Pack",2,9,6),
-		("Single Pack",3,NULL,NULL);
+        ("Full Pack",5,6,1,'2017-01-01 10:00:00','2018-01-01 10:00:00');
+        
+        ("Medium Pack",5,6,2,'2017-01-01 10:00:00','2017-01-06 10:00:00'),
+		("Single Pack",NULL,NULL,3,'2018-01-01 10:00:00','2018-01-01 13:00:00');
+        
 
 -- YYYY-MM-DD HH:MM:SS
+-- Povoamento da tabela "Horário"
+
 INSERT INTO Horário
         (Inicio,Fim)
        VALUES
+			('2017-01-01 10:00:00','2018-01-01 10:00:00'),
+            ('2017-01-01 10:00:00','2017-01-06 10:00:00'),
+            ('2018-01-01 10:00:00','2018-01-01 13:00:00'),
             ('2017-01-01 10:00:00','2017-01-01 20:00:00'),
             ('2017-01-01 12:10:00','2017-01-01 17:30:00'),
-            ('2017-01-01 8:00:00','2017-01-01 10:00:00'),
+            ('2017-01-01 08:00:00','2017-01-01 10:00:00'),
             ('2017-01-02 10:00:00','2017-01-02 14:00:00'),
             ('2017-01-02 10:00:00','2017-01-02 15:00:00'),
             ('2017-01-02 15:00:00','2017-01-02 22:00:00'),
@@ -99,35 +108,38 @@ INSERT INTO Horário
             ('2017-01-02 19:00:00','2017-01-02 22:00:00'),
             ('2017-01-02 20:00:00','2017-01-02 22:00:00');
 
+
 INSERT INTO Contactos
         (Telemóvel,idCliente,Email,Skype)
        VALUES
             (926790525,1,"aviveiros2012@gmail.com","Guilherme...Madeira"),
             (926888525,2,"cesarAugusto@gmail.com","Césarlb"),
-	     (965143652,4,"angeloSousa@hotmail.com","AngSousa"),
-            (966585445,3 "discupulo88@yahoo.com", "Discupulo");
+			(965143652,4,"angeloSousa@hotmail.com","AngSousa"),
+            (966585445,3,"discupulo88@yahoo.com", "Discipulo");
 
+select *from cliente;
 
 INSERT INTO Morada
         (Rua,Nº_porta,idCliente,Localidade)
        VALUES
             ("Rua das Teixeiras",21,1, "Braga"),
-	     ("Rua dos Compinchas",5,2, "Porto"),
+			("Rua dos Compinchas",5,2, "Porto"),
             ("Rua da Maria",32,3, "Lisboa"),
             ("Terra Nostra",4,4, "Alen"),
-			  ("Rua Sequeiros",23,4,"Porto"),
-		("Praça da liberdade",55,5,"Viana do Castelo"),
-		("Reta Traseira",123,6,"Aveiro"),
-		("Avenida dos Corvos",22,7,"Leiria"),
-		("Isengard",1,8,"Terra Media"),
-		("Shire",78,9,"Terra Media"),
-		("colina de palantino",1,10,"Roma"),
-		("rua afonsina",888,11,"Porto"),
-		("rua afonsina",888,12,"Porto"),
-		("Minas Tirith",1,13,"Terra Media"),
-		("Shire",13,14,"Terra Media"),
-		("Mordor",1,14,"Terra Media");
+			("Rua Sequeiros",23,4,"Porto"),
+			("Praça da liberdade",55,5,"Viana do Castelo"),
+			("Reta Traseira",123,6,"Aveiro"),
+			("Avenida dos Corvos",22,7,"Leiria"),
+			("Isengard",1,8,"Terra Media"),
+			("Shire",78,9,"Terra Media"),
+			("colina de palantino",1,10,"Roma"),
+			("rua afonsina",888,11,"Porto"),
+			("rua afonsina",888,12,"Porto"),
+			("Minas Tirith",1,13,"Terra Media"),
+			("Shire",13,14,"Terra Media"),
+			("Mordor",1,14,"Terra Media");
 
+-- Povoamento da tabela "Personal_Trainer"
 INSERT INTO Personal_Trainer
         (idFuncionário,Classificação)
        VALUES
@@ -139,6 +151,7 @@ INSERT INTO Pacote_Tem_Horário_Para_Musculação
     (idPacote,idCliente,Horário_Inicio,Horário_Fim,Horário_Dia)
     VALUES
         (6,5,'10:00:00','20:00:00',"Segunda");
+
 
 
 -- INSERT INTO
