2abbf7ab979f0e798f7ae24dc5aacbfa2a8e8164
CesarAugustoBorges <cesar.a.borges98@gmail.com>
2018-11-21 22:35:34 +0000

    addAulas_To_PlanoDeTreino

--- NPNG_Transations.sql
+++ NPNG_Transations.sql
@@ -14,12 +14,12 @@ DELIMITER %%
 CREATE DEFINER=`root`@`localhost` PROCEDURE `addPT_To_Pacote`(in idPT int, in idPac int, in reclamação VARCHAR(200))
 BEGIN
 	DECLARE oldPT int default null;
-	DECLARE done boolean default False;
+	DECLARE done BOOL default 0;
     DECLARE err BOOL DEFAULT 0;
 	DECLARE id int;
 	DECLARE idsClientes CURSOR FOR SELECT idCliente FROM Cliente_tem_Pacote
 		where idPacote = idPac;
-	DECLARE CONTINUE HANDLER FOR NOT FOUND SET done = True;
+	DECLARE CONTINUE HANDLER FOR NOT FOUND SET done = 1;
 	DECLARE CONTINUE HANDLER FOR SQLEXCEPTION SET err = 1;
     SELECT @oldPT := id_Personal_Trainer from Pacote where idPacote = idPac;
 	OPEN idsClientes;
@@ -31,14 +31,15 @@ BEGIN
 					leave insertLOOP;
 				end if;
 				
-				if oldPT != null then
+				-- if oldPT is not null then
+                SELECT "I am here";
 					INSERT INTO Cliente_teve_Personal_Trainer (idCliente, idPersonal_Trainer, Reclamação)
 						 SELECT * FROM (SELECT id, oldPT, reclamação) AS tmp
 							WHERE NOT EXISTS (
 								SELECT * FROM Cliente_teve_Personal_Trainer 
 									WHERE idCliente = id and idPersonal_Trainer = oldPT
 							) LIMIT 1;
-				END if;        
+				-- END if;        
 				ITERATE insertLOOP;
 			END LOOP;
 			Close idsClientes;
@@ -52,4 +53,41 @@ BEGIN
 END %%
 DELIMITER ;
 
--- SELECT * FROM Pacote;
+
+SELECT * FROM AulasRecomendadas;
+SELECT * FROM Aula;
+call addAulas_To_PlanoDeTreino(1, "5,2", "fitness");
+
+DROP PROCEDURE IF EXISTS `addAulas_To_PlanoDeTreino`;
+
+DELIMITER %%
+CREATE DEFINER=`root`@`localhost` PROCEDURE `addAulas_To_PlanoDeTreino`(in planoTreino int, in idAulas VARCHAR(200), in just VARCHAR(200) )
+BEGIN
+	DECLARE err BOOL DEFAULT 0;
+    DECLARE aula VARCHAR(200);
+	DECLARE i int default 1;
+	DECLARE CONTINUE HANDLER FOR SQLEXCEPTION SET err = 1;
+	if LENGTH(idAulas) < 200 then
+		START Transaction;
+        
+		insertAulas: LOOP
+            SET aula = substring_index(idAulas, "," , -i);
+            SET aula = substring_index(aula, "," , 1);
+			
+            INSERT INTO AulasRecomendadas (idPlano_de_Treino, idAula, Justificação) VALUES
+				(planoTreino, aula, just);
+            
+			if(aula = substring_index(idAulas, "," , 1)) then
+				leave insertAulas;
+			END if;
+			SET i = i + 1;
+			iterate insertAulas;
+		END LOOP;
+        
+		if err then rollback;
+			else commit;
+		END IF;
+        
+    END if;
+END %%;
+DELIMITER ;
