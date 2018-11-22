ad58044344eb5df97cc7b11983afff6ce7c9d937
FallenFoil <luisjrsm@hotmail.com>
2018-11-22 22:38:15 +0000

    Updates povoamento e modelo conceptual

---
+++ NPNG_apaga_povoamento.sql
@@ -0,0 +1,58 @@
+use Npng;
+
+delete from Pacote_tem_Aula
+	where 0=0;
+    
+delete from Cliente_teve_Personal_Trainer
+	where 0=0;
+    
+delete from Morada
+	where 0=0;
+    
+delete from Cliente_tem_Pacote
+	where 0=0;
+    
+delete from AulasRecomendadas
+	where 0=0;
+    
+delete from Cliente_frequentou_Aula_com_Horário
+	where 0=0;
+    
+delete from Aula_tem_Horário
+	where 0=0;
+    
+delete from Pacote_tem_Horário_Para_Musculação
+	where 0=0;
+    
+delete from Funcionário_tem_Horário
+	where 0=0;
+    
+delete from Horário
+	where 0=0;
+    
+delete from Aula
+	where 0=0;
+    
+delete from Contactos
+	where 0=0;
+    
+delete from Cliente
+	where 0=0;
+    
+delete from Plano_de_Treino_Recomenda_Máquina
+	where 0=0;
+    
+delete from Plano_de_Treino
+	where 0=0;
+    
+delete from Pacote
+	where 0=0;
+    
+delete from Personal_Trainer
+	where 0=0;
+    
+delete from Máquina 
+	where 0=0;
+    
+delete from Funcionário
+	where 0=0;
