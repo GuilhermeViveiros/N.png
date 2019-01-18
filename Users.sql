aa5bd62dba1fe3a17c6a35a9aebb0fdfe25960aa
CesarAugustoBorges <cesar.a.borges98@gmail.com>
2019-01-18 10:08:00 +0000

    Correções

--- Users.sql
+++ Users.sql
@@ -20,30 +20,30 @@ select host, user from mysql.user; -- ver os host
 -- Plano de Treino  , AulasRecomendadas , Aulasquefrequentou , Pacote_tem_Horario_Para_Musculação 
 create user 'User'@'localhost' identified by 'user'; -- User user
 -- Pode ver os Pacotes 
-GRANT SELECT ON NPNG.Pacote  TO 'User';
+GRANT SELECT ON Npng.Pacote  TO 'User'@'localhost';
 -- Pode ver as Aulas
-GRANT SELECT ON NPNG.Aula  TO 'User';
+GRANT SELECT ON Npng.Aula  TO 'User'@'localhost';
 -- Pode ver os horários das Aulas
-GRANT SELECT ON NPNG.Aula_tem_Horário  TO 'User';
+GRANT SELECT ON Npng.Aula_tem_Horário  TO 'User'@'localhost';
 -- Pode ver as máquinas
-GRANT SELECT ON NPNG.Máquina  TO 'User';
+GRANT SELECT ON Npng.Máquina  TO 'User'@'localhost';
 
 -- -> Pode ver qualquer um destes métodos através do seu ID
 
 -- Pode ver o seu Pacote
-GRANT EXECUTE ON PROCEDURE Package  TO 'User';
+GRANT EXECUTE ON PROCEDURE Package  TO 'User'@'localhost';
 -- Pode ver seu Plano de Treino
-GRANT EXECUTE ON PROCEDURE TrainningPlan  TO 'User';
+GRANT EXECUTE ON PROCEDURE TrainningPlan  TO 'User'@'localhost';
 -- Pode ver suas AulasRecomendadas
-GRANT EXECUTE ON PROCEDURE RecommendedClasses  TO 'User';
+GRANT EXECUTE ON PROCEDURE RecommendedClasses  TO 'User'@'localhost';
 -- Pode ver as Aulas que frequentou
-GRANT EXECUTE ON PROCEDURE ClassesAttended  TO 'User';
+GRANT EXECUTE ON PROCEDURE ClassesAttended  TO 'User'@'localhost';
 -- Pode ver o horário do seu pacote
-GRANT EXECUTE ON PROCEDURE PackageSchedule  TO 'User';
+GRANT EXECUTE ON PROCEDURE PackageSchedule  TO 'User'@'localhost';
 -- Pode ver o horário para fazer musculação incluido no pacote
-GRANT EXECUTE ON PROCEDURE MuscleSchedule  TO 'User';
+GRANT EXECUTE ON PROCEDURE MuscleSchedule  TO 'User'@'localhost';
 -- Pode ver todas as VIEWS
-GRANT SELECT ON Views.* TO 'User';
+GRANT SELECT ON Views.* TO 'User'@'localhost';
 
 -- ------------------------------------------------------------------------------Funcionário---------------------------------------------------------------------------------------------------
 
@@ -73,9 +73,9 @@ GRANT SELECT , INSERT  ON NPNG.Máquina To 'Gestor';
 create user 'Personal_Trainer' identified by 'Personal_Trainer';
 
 -- Pode mexer no Plano de Treino / Plano de treino recomenda máquina/ Aulas Recomendadas
-GRANT SELECT,INSERT,UPDATE ON NPNG.Plano_de_Treino TO 'Personal_Trainer';
-GRANT SELECT,INSERT,UPDATE ON NPNG.AulasRecomendadas TO 'Personal_Trainer';
-GRANT SELECT,INSERT,UPDATE ON NPNG.Plano_de_Treino_Recomenda_Máquina TO 'Personal_Trainer';
+GRANT SELECT,INSERT,UPDATE ON Npng.Plano_de_Treino TO 'Personal_Trainer';
+GRANT SELECT,INSERT,UPDATE ON Npng.AulasRecomendadas TO 'Personal_Trainer';
+GRANT SELECT,INSERT,UPDATE ON Npng.Plano_de_Treino_Recomenda_Máquina TO 'Personal_Trainer';
 
 -- Para estes métodos só precisamos fornecer o id do Personal Trainer
 
@@ -92,7 +92,7 @@ GRANT EXECUTE ON PROCEDURE ActualClientsOfPT TO 'Personal_Trainer';
 create user 'Programador' identified by 'programador';
 
 
-GRANT CREATE,DELETE,INSERT,SELECT ON NPNG.* TO 'Programador';
-GRANT EXECUTE, UPDATE ON VIEW TO 'Programador';
+GRANT CREATE,INSERT,SELECT ON Npng.* TO 'Programador';
+-- GRANT EXECUTE, UPDATE ON VIEW TO 'Programador';
 
 
